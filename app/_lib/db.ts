a12cb865d729d88656d5eb67934749e9392f32cf
Gaiete44 <rafmasolijn44@gmail.com>
2024-11-22 09:32:30 +0100

    Update db.ts

--- app/_lib/db.ts
+++ app/_lib/db.ts
@@ -1,5 +1,5 @@
 // app/_lib/db.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/prisma/generated/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
